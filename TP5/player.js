18520992d34591fb8656d5f764cbe80772d57504
LeMeubleRandom <antoinedbc2005@gmail.com>
2026-01-16 10:04:04 +0100

    tp6

--- TP5/player.js
+++ TP5/player.js
@@ -1,150 +1,146 @@
 class Player {
-  constructor(name, skin, coord) {
+  constructor(id, name, skin, coord) {
+    this.id = id;
     this.name = name;
     this.skin = skin;
-    this.coord = coord;
+    this.coordX = coord[0];
+    this.coordY = coord[1];
+
+    this.lvl = 1;
+    this.hp = 100;
+    this.maxHp = 100;
+    this.speed = 0.2;
+
+    this.direction = directions.south;
+    this.isWalking = false;
+    this.isAttacking = false;
+    this.isDying = false;
+    this.isDead = false;
+
+    this.walkSpriteIndex = 0;
+    this.walkSpritesNumber = 9;
+    this.currentWalkSpriteStep = 0;
+    this.walkSpriteDuration = 3;
+
+    this.attackSpriteIndex = 0;
+    this.attackSpritesNumber = 6;
+    this.currentAttackSpriteStep = 0;
+    this.attackSpriteDuration = 0;
+
+    this.deathSpriteIndex = 0;
+    this.deathSpritesNumber = 6;
+    this.currentDeathSpriteStep = 0;
+    this.deathSpriteDuration = 5;
   }
-  level = 0;
-  attack = 0;
-  cooldown = 0;
-  speed = 0;
-  hp = 50;
-  max_hp = 50;
-  hpPerS = 0;
-  currentCooldown = 0;
-  direction = 0;
-
-  dead = false;
-  attacking = false;
-  moving = false;
-
-  walkSpriteDuration = 2;
-  walkSpriteIndex = 0;
-  walkSpriteNumber = 9;
-  currentStep = 0;
 
   animate() {
-    if (this.moving) {
-      this.currentStep++;
-      if (this.currentStep >= this.walkSpriteDuration) {
-        this.currentStep = 0;
+    // If the player is walking
+    if (this.isWalking) {
+      // Reset attack sprite index and current attack sprite's step to 0 as we may have interrupted an attack animation
+      this.attackSpriteIndex = 0;
+      this.currentAttackSpriteStep = 0;
+
+      // Increment the current walk sprite step to display the current walking animation sprite for the right number of frames
+      this.currentWalkSpriteStep++;
+      // If we displayed it for long enough
+      if (this.currentWalkSpriteStep >= this.walkSpriteDuration) {
+        // Then we reset our step and increment our sprite index to go for the next sprite in the animation
+        this.currentWalkSpriteStep = 0;
         this.walkSpriteIndex++;
       }
-      if (this.walkSpriteIndex >= this.walkSpriteNumber) {
+      // If we reach the last sprite in the animation and try going for the next one
+      if (this.walkSpriteIndex >= this.walkSpritesNumber) {
+        // We reset our index to display the first sprite of the animation instead, forming a looping animation
         this.walkSpriteIndex = 0;
       }
-      console.log(
-        "En train d'attaquer :",
-        this.attacking,
-        "\nEn train de bouger :",
-        this.moving,
-        "\nEn train de mourir :",
-        this.dead,
-        "\nSprite N°",
-        this.walkSpriteIndex
-      );
-      //en fonction de la direction
-      //prendre ce paramêtre de direction pour calculer l'une des 4 lignes de déplacement
-    } else if (this.attacking) {
-      this.currentStep++;
-      if (this.currentStep >= this.walkSpriteDuration) {
-        this.currentStep = 0;
-        this.walkSpriteIndex++;
-      }
-      if (this.walkSpriteIndex >= this.walkSpriteNumber) {
-        this.walkSpriteIndex = 0;
-      }
-      console.log(
-        "En train d'attaquer :",
-        this.attacking,
-        "\nEn train de bouger :",
-        this.moving,
-        "\nEn train de mourir :",
-        this.dead,
-        "\nSprite N°",
-        this.walkSpriteIndex
-      );
-      //en fonction de la direction
-      //prendre ce paramêtre de direction pour calculer l'une des 4 lignes d'attaque
-    } else if (this.dead) {
-      this.currentStep++;
-      if (this.currentStep >= this.walkSpriteDuration) {
-        this.currentStep = 0;
-        this.walkSpriteIndex++;
-      }
-      console.log(
-        "En train d'attaquer :",
-        this.attacking,
-        "\nEn train de bouger :",
-        this.moving,
-        "\nEn train de mourir :",
-        this.dead,
-        "\nSprite N°",
-        this.walkSpriteIndex
-      );
-      //en fonction de la direction
-      //prendre ce paramêtre de direction pour calculer l'une des 4 lignes de mort
-    } else {
-      this.currentStep = 0;
+    }
+    // If the player is attacking, or the attack animation started already
+    else if (
+      this.isAttacking ||
+      this.currentAttackSpriteStep > 0 ||
+      this.attackSpriteIndex > 0
+    ) {
+      // Reset the walking animation variables as we may have interrupted a walking animation
+      this.currentWalkSpriteStep = 0;
       this.walkSpriteIndex = 0;
-      console.log(
-        "En train d'attaquer :",
-        this.attacking,
-        "\nEn train de bouger :",
-        this.moving,
-        "\nEn train de mourir :",
-        this.dead
-      );
-      //en fonction de la direction
-      //prendre la première frame de marche
+
+      // Increment the current attack sprite step to display the current attacking animation sprite for the right number of frames
+      this.currentAttackSpriteStep++;
+      // If we displayed it for long enough
+      if (this.currentAttackSpriteStep >= this.attackSpriteDuration) {
+        // Then we reset our step and increment our sprite index to go for the next sprite in the animation
+        this.currentAttackSpriteStep = 0;
+        this.attackSpriteIndex++;
+      }
+      // If we reach the last sprite in the animation and try going for the next one
+      if (this.attackSpriteIndex >= this.attackSpritesNumber) {
+        /*
+                    We reset our sprite index to 0 for the next attack animation.
+                    This reset does not serve a looping purpose like for the walking animation ;
+                    The else if condition leading to our attack animation needs at least one of currentAttackSpriteStep and attackSpriteIndex to be > 0
+                    When we reach the if we are in right now, currentAttackSpriteStep is always equal to 0
+                    When we'll get out of it, attackSpriteIndex will be set to 0 too
+                    This will signify that the animation is over, and the else if condition leading to our attack animation should not trigger anymore.
+                    This mecanism is implemented to avoid playing several attacking animations in a row (graphically incoherent with our attack cooldown).
+                    The only exception would be for the server to maintain isAttacking at true even after the end of the animation.
+                    This is not supposed to happen as i programmed the server to maintain isAttacking at true for a very short amount of time.
+                */
+
+        this.attackSpriteIndex = 0;
+      }
+    }
+    // If the player is dying, or the dying animation already started
+    else if (
+      this.isDying ||
+      this.currentDeathSpriteStep > 0 ||
+      this.deathSpriteIndex > 0
+    ) {
+      // No resets here as this will be the last animation of the player (ce n'est qu'un au revoir bebou).
+      // Increment the current death sprite step to display the current death animation sprite for the right number of frames
+      this.currentDeathSpriteStep++;
+      // If we displayed it for long enough
+      if (this.currentDeathSpriteStep >= this.deathSpriteDuration) {
+        // Then we reset our step and increment our sprite index to go for the next sprite in the animation
+        this.currentDeathSpriteStep = 0;
+        this.deathSpriteIndex++;
+      }
+      // If we reach the last sprite in the animation and try going for the next one
+      if (this.deathSpriteIndex >= this.deathSpritesNumber) {
+        // It means the animation is over ; we set isDead at true to keep the player from being displayed in next frames
+        // e.g, we make it disappear like in the good old URSS days.
+        this.isDead = true;
+      }
+    }
+    // If the player is idle
+    else {
+      // We just select the first sprite of its walking animation
+      this.walkSpriteIndex = 0;
     }
   }
 
-  showHealthBar() {
-    console.log(`${this.hp}/${this.max_hp}`);
-  }
+  updatePlayer(updateData) {
+    this.name = updateData.name;
+    this.lvl = updateData.lvl;
+    this.hp = updateData.hp;
+    this.maxHp = updateData.maxHp;
+    this.attackCooldown = updateData.attackCooldown;
+    this.currentAttackCooldown = updateData.currentAttackCooldown;
+    this.speed = updateData.speed;
 
-  takeDamage(damage) {
-    this.hp -= damage;
-    this.takeDamageAnim(damage);
-  }
-
-  takeDamageAnim(damage) {
-    console.log(`Vous avez subis ${damage}`);
-  }
-
-  levelUp() {
-    this.level++;
-    this.levelUpAnim(level);
-  }
-
-  levelUpAnim(level) {
-    console.log(`Vous avez passé un niveau\nNiveau ${level}`);
-  }
-
-  updatePlayer(newData) {
-    this.name = newData.name;
-    this.skin = newData.skin;
-    this.level = newData.level;
-    this.attack = newData.attack;
-    this.cooldown = newData.cooldown;
-    this.currentCooldown = newData.currentCooldown;
-    this.speed = newData.speed;
-    this.hp = newData.hp;
-    this.max_hp = newData.max_hp;
-    this.hpPerS = newData.hpPerS;
-    this.coord = newData.coord;
-    this.direction = newData.direction;
-    this.dead = newData.dead;
-    this.attacking = newData.attacking;
-    this.moving = newData.moving;
-    this.currentStep = newData;
-    this.walkSpriteDuration = newData.walkSpriteDuration;
-    this.walkSpriteIndex = newData.walkSpriteIndex;
-    this.walkSpriteNumber = newData.walkSpriteNumber;
-    this.isAlive(newData.hp);
+    this.direction = updateData.direction;
+    this.isAttacking = updateData.isAttacking;
+    this.isWalking = updateData.isWalking;
+    this.isDying = updateData.isDying;
+    this.skinPath = updateData.skinPath;
   }
 }
+
+const directions = {
+  north: 0,
+  east: 1,
+  south: 2,
+  west: 3,
+};
 
 let player1 = new Player("test", 1);
 let player2 = new Player("test2", 2);
--- TP6/portail.js
+++ TP6/portail.js
@@ -1,3 +1,131 @@
-const player_name = document.querySelector("#username");
+const bouton = document.querySelector("#userinfo");
 
-console.log(player_name);
+bouton.addEventListener("submit", function (event) {
+  event.preventDefault();
+  const usernameValue = document.querySelector("#username").value;
+  const linkValue = document.querySelector("#link").value;
+  const radioValue =
+    document.querySelector('input[name="skin"]:checked')?.value ?? "noskin";
+  if (usernameValue === null || linkValue === null || radioValue === "noskin")
+    alert("Remplissez tous les champs");
+  else {
+    const skinNumber = radioValue.replace(/\D/g, "");
+    console.log(usernameValue, linkValue, skinNumber);
+    localStorage.setItem("username", usernameValue);
+    localStorage.setItem("link", linkValue);
+    localStorage.setItem("skin", skinNumber);
+  }
+});
+
+for (let i = 1; i < 30; i++) {
+  const div = document.createElement("div");
+  div.id = `dskin${i}`;
+
+  const input = document.createElement("input");
+  input.type = "radio";
+  input.id = `iskin${i}`;
+  input.value = `skin ${i}`;
+  input.name = "skin";
+  input.className = "iskin";
+
+  const label = document.createElement("label");
+  label.htmlFor = `iskin${i}`;
+  label.id = `lskin${i}`;
+
+  div.appendChild(input);
+  div.appendChild(label);
+
+  document.getElementById("userskin").appendChild(div);
+
+  const newImg = document.createElement("canvas");
+  newImg.setAttribute("id", `skin${i}`);
+  newImg.className = "peau";
+  document.getElementById(`lskin${i}`).appendChild(newImg);
+  const canvas = document.querySelector(`#skin${i}`);
+  const context = canvas.getContext("2d");
+  let img = new Image();
+
+  img.onload = function () {
+    var cropX = 0;
+    var cropY = 64 * 10;
+    var cropWidth = 64;
+    var cropHeight = 64;
+
+    canvas.style.width = cropWidth;
+    canvas.style.height = cropHeight;
+
+    context.drawImage(
+      img,
+      cropX,
+      cropY,
+      cropWidth,
+      cropHeight,
+      0,
+      0,
+      canvas.width,
+      canvas.height
+    );
+  };
+  img.src = `assets/${i}.png`;
+  canvas.imageSource = img;
+}
+
+let test = document.querySelectorAll(".peau");
+
+/*test.forEach((element) => {
+  element.addEventListener("mouseenter", function () {
+    let img = element.imageSource;
+    const context = element.getContext("2d");
+    element.style.background = "black";
+
+    context.clearRect(0, 0, 64, 64);
+
+    var cropX = 0;
+    var cropY = 64 * 15;
+    var cropWidth = 64;
+    var cropHeight = 64;
+
+    element.style.width = cropWidth;
+    element.style.height = cropHeight;
+
+    context.drawImage(
+      img,
+      cropX,
+      cropY,
+      cropWidth,
+      cropHeight,
+      0,
+      0,
+      element.width,
+      element.height
+    );
+  });
+
+  element.addEventListener("mouseout", function () {
+    let img = element.imageSource;
+    const context = element.getContext("2d");
+
+    context.clearRect(0, 0, 64, 64);
+
+    var cropX = 0;
+    var cropY = 64 * 10;
+    var cropWidth = 64;
+    var cropHeight = 64;
+
+    element.style.width = cropWidth;
+    element.style.height = cropHeight;
+
+    context.drawImage(
+      img,
+      cropX,
+      cropY,
+      cropWidth,
+      cropHeight,
+      0,
+      0,
+      element.width,
+      element.height
+    );
+  });
+});
+*/
